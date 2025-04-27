f16bbbbe7104e9fdb395a5c3e281d651febc2315
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-04-27 18:32:03 -0300

    Update CurrencyPriceClient.java

--- cotacao/src/main/java/org/br/mineradora/gabryel/client/CurrencyPriceClient.java
+++ cotacao/src/main/java/org/br/mineradora/gabryel/client/CurrencyPriceClient.java
@@ -1,11 +1,19 @@
 package org.br.mineradora.gabryel.client;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import org.br.mineradora.gabryel.dto.CurrencyPriceDTO;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @Path("/currency-price")
 @RegisterRestClient
 @ApplicationScoped
 public interface CurrencyPriceClient {
+
+    @GET
+    @Path("/{pair}")
+    CurrencyPriceDTO getCurrencyPrice(@PathParam("pair") String pair);
+    
 }
