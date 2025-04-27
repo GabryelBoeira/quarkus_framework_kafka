7881b2c8636341f8f679494243d6bc3f81adcca4
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-04-27 20:57:57 -0300

    Configurando o kafka para a cotação

---
+++ cotacao/src/main/java/org/br/mineradora/gabryel/message/KafkaEvents.java
@@ -0,0 +1,25 @@
+package org.br.mineradora.gabryel.message;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.br.mineradora.gabryel.dto.QuotationDTO;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@ApplicationScoped
+public class KafkaEvents {
+
+    private final Logger LOG = LoggerFactory.getLogger(KafkaEvents.class);
+
+    @Channel("quotation-channel")
+    Emitter<QuotationDTO> quotationRequestEmitter;
+
+    public void sendNewKafkaEvent(QuotationDTO quotationDTO) {
+
+        LOG.info("--- Sending quotation {} ---", quotationDTO);
+        quotationRequestEmitter.send(quotationDTO).toCompletableFuture().join();
+    }
+
+}
