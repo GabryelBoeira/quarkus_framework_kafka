1384a0864202d651cf59f8fa7d79fb013e0ea2bd
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-05-11 00:09:31 -0300

    Add OIDC token propagation dependency and update report endpoint
    Added `quarkus-rest-client-oidc-token-propagation` dependency to `pom.xml` files in both `proposta` and `relatorio` modules to enable token propagation in REST clients. Removed unnecessary `@Produces` annotation from the `generateReport` method in `OpportunityController`, as it is no longer required.

--- relatorio/src/main/java/org/br/mineradora/gabryel/controller/OpportunityController.java
+++ relatorio/src/main/java/org/br/mineradora/gabryel/controller/OpportunityController.java
@@ -5,22 +5,14 @@ import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.ServerErrorException;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.br.mineradora.gabryel.service.OpportunityService;
 import org.eclipse.microprofile.jwt.JsonWebToken;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.time.LocalDate;
 
 @Path("/api/opportunity")
 @Authenticated
 public class OpportunityController {
-
-    private static final Logger LOG = LoggerFactory.getLogger(OpportunityController.class);
 
     @Inject
     OpportunityService opportunityService;
@@ -30,7 +22,6 @@ public class OpportunityController {
 
     @GET
     @Path("/report")
-    @Produces(MediaType.APPLICATION_OCTET_STREAM)
     @RolesAllowed({"user", "manager"})
     public Response generateReport() {
         try {
