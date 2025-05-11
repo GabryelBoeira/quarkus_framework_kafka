a0d424963d6d99008a353531b2b3fffbf4c08327
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-05-11 00:00:22 -0300

    Add authentication and role-based access to report endpoint
    Implemented role-based access control using `@RolesAllowed` for the report generation endpoint, restricting access to users with "user" or "manager" roles. Added `@Authenticated` annotation to secure the `OpportunityController`. Updated the method to use `JsonWebToken` for enhanced security checks.

--- relatorio/src/main/java/org/br/mineradora/gabryel/controller/OpportunityController.java
+++ relatorio/src/main/java/org/br/mineradora/gabryel/controller/OpportunityController.java
@@ -1,5 +1,7 @@
 package org.br.mineradora.gabryel.controller;
 
+import io.quarkus.security.Authenticated;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -8,12 +10,14 @@ import jakarta.ws.rs.ServerErrorException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.br.mineradora.gabryel.service.OpportunityService;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 
 @Path("/api/opportunity")
+@Authenticated
 public class OpportunityController {
 
     private static final Logger LOG = LoggerFactory.getLogger(OpportunityController.class);
@@ -21,20 +25,19 @@ public class OpportunityController {
     @Inject
     OpportunityService opportunityService;
 
+    @Inject
+    JsonWebToken jwt;
+
     @GET
     @Path("/report")
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    @RolesAllowed({"user", "manager"})
     public Response generateReport() {
         try {
-            return Response
-                    .ok(opportunityService.generateCsvOpportunityReport(), MediaType.APPLICATION_OCTET_STREAM)
-                    .header("content-disposition", "attachment; filename = " + LocalDate.now() + "_opportunities.csv")
-                    .build();
+            return Response.ok(opportunityService.generateOpportunityData()).build();
         } catch (ServerErrorException e) {
-            LOG.error("fail to generate report csv: {}", e.getMessage());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
-
 
 }
